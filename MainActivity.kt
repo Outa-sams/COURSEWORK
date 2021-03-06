ced1a0e7eb35b6c0f84af0ff7c126d634e94e172
Samson Outa <50982609+Outa-sams@users.noreply.github.com>
2021-03-06 17:19:08 +0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+
+fun main(args: Array<String>){
+
+    var outa: Array<String> = arrayOf("CSC3104 Computer Networks and Security, CSC3112 Data Communications and Networks, CSC3115 MobileProgramming, CSC3 Artificial Intelligence,CSC3113 Entreprenuership, CSC3117 Automata Complexity and Computability")
+
+    outa.forEach{System.out.println(it)}
+
+}
