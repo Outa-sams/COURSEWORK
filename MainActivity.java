e3209f450a3f26c08f046596e4755c2861d35cf1
Samson Outa <50982609+Outa-sams@users.noreply.github.com>
2021-03-06 17:16:52 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfirstassignment;
+
+
+public class MainActivity {
+    public static void main(String[] args) {
+        System.out.println("MY REGISTERED EXAM COURSE UNITS:");
+        System.out.println("1. CSC3115: MOBILE PROGRAMMING");
+        System.out.println("2. CSC3112: DATA COMMUNICATIONS AND NETWORKS");
+        System.out.println("3. CSC3104: COMPUTER AND NETWORK SECURITY");
+        System.out.println("4. CSC3116: ARTIFICIAL INTELLIGENCE");
+        System.out.println("5. CSC3113: ENTREPRENEURSHIP");
+        System.out.println("6. CSC3117: AUTOMATA, COMPLEXITY AND COMPUTABILITY");
+    }
+}
+
