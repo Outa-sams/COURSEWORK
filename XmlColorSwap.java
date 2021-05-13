8925ad3b9ea02d889ee73eddce4144a1e1472f79
Samson Outa <50982609+Outa-sams@users.noreply.github.com>
2021-05-13 13:40:47 +0300

    Add files via upload
    this is the broad cast sender

--- XmlColorSwap.java
+++ XmlColorSwap.java
@@ -3,25 +3,28 @@ package must.ac.csce.outasamson.xmlcolorswap;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class XmlColorSwap extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.btn);
+        Button button = (Button) findViewById(R.id.sending);
         TextView textview = (TextView) findViewById(R.id.textcolor);
-
         final Random random= new Random();
+        button= findViewById(R.id.sending);
+
 
         button.setOnClickListener((v )-> {
             float red= random.nextFloat();
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
             float blue= random.nextFloat();
             textview.setText("r"+String.valueOf(red)+"   g"+String.valueOf(green+" b"+String.valueOf(blue)  ));
             textview.setTextColor(Color.rgb(red,green,blue));
+            String message= "BroadcastMessage";
+            Intent intent= new Intent();
+            intent.setAction("must.codes.programcolorswap");
+            intent.putExtra("message",message);
+            intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+            sendBroadcast(intent);
+
     });
 }
 }
